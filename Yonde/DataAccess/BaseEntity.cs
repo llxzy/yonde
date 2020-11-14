31f2a7457514983249df9c1ebb9b5d6fce6a2c0a
llxzy <snowcrash@tuta.io>
2020-11-14 22:57:42 +0100

    started dal

---
+++ Yonde/DataAccess/BaseEntity.cs
@@ -0,0 +1,7 @@
+namespace DataAccess
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Yonde/DataAccess/YondeDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class YondeDbContext : DbContext
+    {
+        
+    }
+}
